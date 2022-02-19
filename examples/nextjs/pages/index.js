8920e6491d22ce9579a4ef73e63478e2d0f38a58
reassemblydev <test@reassembly.co>
2022-02-18 18:28:09 -0600

    Initial methin around

--- examples/nextjs/pages/index.js
+++ examples/nextjs/pages/index.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
 
@@ -11,12 +10,11 @@ export default function Index() {
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
+          — R E A S S E M B L Y —
         </Typography>
         <Link href="/about" color="secondary">
           Go to the about page
         </Link>
-        <ProTip />
         <Copyright />
       </Box>
     </Container>
